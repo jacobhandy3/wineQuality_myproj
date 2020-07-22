dcab2eef1ca94bdfc3e120df3f776f3ac30d8814
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2020-07-22 15:14:16 -0500

    a few changes,same accuracy
    I rearranged the order of loops with the categorical values first. Then they are ordered least to most range of values for parameters. After a period of 2 observations I found that maxLeaf resulted in 0% accuracy at value 2 and a minimum impurity decrease higher than 0.0 results in 0% accuracy. I proceeded to set these parameters to default values until further research can be completed. there is a printing error in the 4th picture with minLeaf because I accidently set the minLeaf key's value to the name of the variable,not the current minLeaf value. The minLeaf value that results in 59.55% accuracy is 1.

--- src/main.py
+++ src/main.py
@@ -11,42 +11,41 @@ dataset = formatData(path=path, head=header, indexCol=indexCol)
 """ NNanalysis(path=path, dataset=dataset, Xmax=Xmax,
            labelCol=labelCol, classNum=classNum)
  """
-high_score = {"score":0,"depth":0,"impurity":0,"maxLeaf":0,"feat":None,"split":0,"minLeaf":0,"crit":"gini"}
+high_score = {"score":0,"depth":0,"impurity":0,"maxLeaf":0,"feat":None,"minSplit":0,"minLeaf":0,"crit":"gini"}
 depth = list(range(1,51,1))                 #50
-impurity = np.arange(0.0,1.1,0.1)             #11
+impurity = 0.0            #1
 maxLeaf = [None,2]                                #2
 feat = [None,"sqrt","log2"]            #3
-minSplit = list(range(2,101,1))                                 #100
-minLeaf = 2                              #
+minSplit = list(range(2,26,1))                                 #24
+minLeaf = [1,2]                              #2
 crit = ["gini", "entropy"]              #2
 count = 0
-""" for c in crit:  #2
+for c in crit:  #2
     for f in feat: #3
-        for d in depth: #50
-            for i in impurity:  #11
-                for maL in maxLeaf: #2
-                    for ms in minSplit: #100
-                        count+=1
-                        print("iteration #: ", count)
-                        score = treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
-                                maxDepth=d,minImpurity=i,maxLeaf=maL,maxFeat=f,
-                                minSplit=ms,minLeaf=minLeaf,criterion=c)
-                        print("parameters: ",c," / ",f," / ",d," / ",i," / ",maL," / ",ms," / ",minLeaf)
-                        if score > high_score["score"]:
-                            high_score["score"] = score
-                            high_score["depth"] = d
-                            high_score["impurity"] = i
-                            high_score["feat"] = f
-                            high_score["crit"] = c
-                            high_score["maxLeaf"] = maL
-                            high_score["minSplit"] = ms
-                            high_score["minLeaf"] = minLeaf """
-print(treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
+        for miL in minLeaf: #2
+            for ms in minSplit: #24
+                for d in depth: #50
+                    count+=1
+                    print("iteration #: ", count)
+                    score = treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
+                            maxDepth=d,minImpurity=0.0,maxLeaf=None,maxFeat=f,
+                            minSplit=ms,minLeaf=miL,criterion=c)
+                    print("parameters: ",c," / ",f," / minLeaf:",miL," / minSplit",ms," / depth:",d," / maxLeaf",None," / minImpurity",0.0)
+                    if score > high_score["score"]:
+                        high_score["score"] = score
+                        high_score["depth"] = d
+                        high_score["impurity"] = 0.0
+                        high_score["feat"] = f
+                        high_score["crit"] = c
+                        high_score["maxLeaf"] = None
+                        high_score["minSplit"] = ms
+                        high_score["minLeaf"] = miL
+""" print(treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
                                 maxDepth=24,minImpurity=0.0,maxLeaf=None,maxFeat="sqrt",
-                                minSplit=2,minLeaf=1,criterion="gini"))
-""" for keys,values in high_score.items():
+                                minSplit=2,minLeaf=2,criterion="gini")) """
+for keys,values in high_score.items():
     print(keys)
-    print(values) """
+    print(values)
 #summarize history for accuracy
 # plt.plot(accuracies)
 # plt.title('model accuracy')
