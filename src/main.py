2c6334853910c5d4b43e44e44b08520d0b0754e9
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2020-07-22 11:05:33 -0500

    new changes for unconfigured paramters
    changed the automated training to include unconfigured paramters for Decision Tree. added '2' as an option for max Leaf. added percent sample splits with float values to sample split and min Leaf split, but received worse accuracy at 40.3%. Will go back to whole numbers for the variables.

--- src/main.py
+++ src/main.py
@@ -12,26 +12,32 @@ dataset = formatData(path=path, head=header, indexCol=indexCol)
            labelCol=labelCol, classNum=classNum)
  """
 high_score = {"score":0,"depth":1,"impurity":0,"maxLeaf":2,"feat":None,"split":2,"minLeaf":2,"crit":"gini"}
-depth = list(range(1,50,1))                 #50
+depth = list(range(1,51,1))                 #50
 impurity = np.arange(0.0,1.1,0.1)             #11
-maxLeaf = None                                 #not config
+maxLeaf = [None,2]                                #2
 feat = [None,"sqrt","log2"]            #3
-split = 2                                 #not config
-minLeaf = 1                                 #not config
+split = np.arange(0.1,0.6,0.1)                                 #5
+minLeaf = np.arange(0.05,0.55,0.05)                                 #10
 crit = ["gini", "entropy"]              #2
 for c in crit:  #2
     for f in feat: #3
-        for d in depth: #50
-            for i in impurity:  #11
-                score = treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
-                        maxDepth=d,minImpurity=i,maxLeaf=maxLeaf,maxFeat=f,
-                        minSplit=split,minLeaf=minLeaf,criterion=c)
-                if score > high_score["score"]:
-                    high_score["score"] = score
-                    high_score["depth"] = d
-                    high_score["impurity"] = i
-                    high_score["feat"] = f
-                    high_score["crit"] = c
+        for maL in maxLeaf: #2
+            for d in depth: #50
+                for i in impurity:  #11
+                    for s in split: #5
+                        for miL in minLeaf: #10
+                            score = treeAnalysis(data=dataset, Xmax=Xmax, labelCol=labelCol,
+                                    maxDepth=d,minImpurity=i,maxLeaf=maL,maxFeat=f,
+                                    minSplit=s,minLeaf=miL,criterion=c)
+                            if score > high_score["score"]:
+                                high_score["score"] = score
+                                high_score["depth"] = d
+                                high_score["impurity"] = i
+                                high_score["feat"] = f
+                                high_score["crit"] = c
+                                high_score["maxLeaf"] = maL
+                                high_score["split"] = s
+                                high_score["minLeaf"] = miL
 
 for keys,values in high_score.items():
     print(keys)
